16022b6a17f9d7eff28cd3205e7b0e50dd8a592f
Luan <114256871+Luan067@users.noreply.github.com>
2025-11-14 20:10:13 -0300

    Enhance Slide class with movement tracking

--- js/slide.js
+++ js/slide.js
@@ -1,19 +1,34 @@
-import debounce from './debounce.js'
+import debounce from "./debounce.js";
 export default class Slide {
   constructor(slide, container) {
     this.slide = document.querySelector(slide);
     this.container = document.querySelector(container);
+    this.dist = { finalPos: 0, startX: 0, movement: 0 };
   }
 
   onStart(event) {
     event.preventDefault();
+    this.dist.startX = event.clientX;
     this.container.addEventListener("mousemove", this.onMove);
   }
 
+  updatePosition(clientX) {
+    this.dist.movement = this.dist.startX - clientX;
+    return this.dist.finalPos - this.dist.movement;
+  }
+
+  moveSlide(distX) {
+    this.dist.movePosition = distX;
+    this.slide.style.transform = `translate3d(${distX}px, 0, 0)`;
+  }
+
   onMove(event) {
+    const finalPosition = this.updatePosition(event.clientX);
+    this.moveSlide(finalPosition);
   }
 
   onEnd(event) {
+    this.dist.finalPos = this.dist.movePosition;
     this.container.removeEventListener("mousemove", this.onMove);
   }
 
